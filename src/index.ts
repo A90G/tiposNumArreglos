4a61fce22ea87ac9f5df93e44f2eb0517cdfbca2
A90G <104040612+A90G@users.noreply.github.com>
2022-05-20 21:18:14 -0300

    commit no muestra el mensaje en el lugar indicado

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
-import "./styles.css";
+//almacene arreglo de dimensión N y muestre cuántos son + y cuántos -
+let dimensionArreglo: number = Number(
+  prompt("Ingrese la dimensión del arreglo")
+);
+let arreglo: number[] = new Array(dimensionArreglo);
+let indice: number;
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+for (indice = 0; indice < dimensionArreglo; indice++) {
+  arreglo[indice] = Number(
+    prompt(`Ingrese el número que va en la posición ${indice}`)
+  );
+  console.log(
+    `El número ingresado en la posición ${indice} es: ${arreglo[indice]}`
+  );
+}
+if (arreglo[indice] === 0) {
+  let cero: number = 0;
+  cero++;
+  console.log(`La cantidad de ceros es: ${cero}`);
+} else {
+  if (arreglo[indice] > 0) {
+    let positivo: number = 0;
+    positivo++;
+    console.log(`La cantidad de positivos es: ${positivo}`);
+  } else {
+    let negativo: number = 0;
+    negativo++;
+    console.log(`La cantidad de negativos es: ${negativo}`);
+  }
+}
